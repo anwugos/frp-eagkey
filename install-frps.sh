f001f3dc02d46e01ad3502e82b4a26aed2b0093b
anwugos <65577106+anwugos@users.noreply.github.com>
2023-06-07 15:29:47 +0800

    Update install-frps.sh

--- install-frps.sh
+++ install-frps.sh
@@ -3,7 +3,7 @@ PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 ###export###
 export PATH
 export FRPS_VER=0.48.0
-export FRPS_INIT="https://raw.githubusercontent.com/MvsCode/frps-onekey/master/frps.init"
+export FRPS_INIT="https://raw.githubusercontent.com/anwugos/frp-eagkey/master/frps.init"
 export gitee_download_url="https://gitee.com/mvscode/frps-onekey/releases"
 export github_download_url="https://github.com/fatedier/frp/releases"
 #======================================================================
